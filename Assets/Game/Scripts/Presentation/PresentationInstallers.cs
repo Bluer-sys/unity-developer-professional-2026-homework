00a8abf7c0117caa49e34ff503c702b506c3e5f4
Ilya <79748538+Bluer-sys@users.noreply.github.com>
2026-03-14 10:27:44 +0300

    Asmdefs

---
+++ Assets/Game/Scripts/Presentation/PresentationInstallers.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Zenject;
+
+namespace Game.Presentation
+{
+    [CreateAssetMenu(fileName = "PresentationInstallers", menuName = "Zenject/New PresentationInstallers")]
+    public sealed class PresentationInstallers : ScriptableObjectInstaller
+    {
+        public override void InstallBindings()
+        {
+            //TODO:
+        }
+    }
+}
